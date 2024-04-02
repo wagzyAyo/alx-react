bb532197a7b5170898a82e427828955e2dd8a365
Ayo <talktojmcvibes@gmail.com>
2024-04-02 20:25:52 +0100

    task-0

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,21 @@
+import logo from './holberton-logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <div className='App-header'>School dashboard</div>
+     </header>
+     <body>
+      <div className='App-body'> <p>Login to access the full dashboard</p></div>
+     </body>
+     <footer>
+      <div><p>Copyright 2020 - holberton School</p></div>
+     </footer>
+     
+    </div>
+  );
+}
+
+export default App;
