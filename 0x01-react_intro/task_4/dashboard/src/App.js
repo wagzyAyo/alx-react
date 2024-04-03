917e8c01f2a5144b077d232f0ff2722f130e1fcd
Ayo <talktojmcvibes@gmail.com>
2024-04-03 16:18:34 +0100

    task_4 build

---
+++ 0x01-react_intro/task_4/dashboard/src/App.js
@@ -0,0 +1,33 @@
+import logo from './holberton-logo.jpg';
+import './App.css';
+import { getFullYear , getFooterCopy} from './utils'
+
+function App() {
+  return (
+    <div className="App">
+        <div className="App-header">
+          <div><img src={logo} alt="logo" /></div>
+          <div><h1>School dashboard</h1></div>
+      </div>
+      <hr></hr>
+      <div className="App-body">
+        <p>
+          Login to access the full dashboard
+        </p>
+        <label htmlFor='email'>Email:</label>
+        <input type='email' ></input>
+        <label htmlFor='password'>Password:</label>
+        <input type='password' ></input>
+        <button ttpe='submit'>OK</button>
+      </div>
+      <hr></hr>
+      <div className="App-footer">
+        <em>
+        <p>Copyright {getFullYear()}- {getFooterCopy(true)}</p>
+        </em>
+      </div>
+    </div>
+  );
+}
+
+export default App
