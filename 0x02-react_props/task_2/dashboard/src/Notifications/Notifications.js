8b705a08c2cd8702d1d12f33cd685b98c7161b2d
Ayo <talktojmcvibes@gmail.com>
2024-04-07 10:32:11 +0100

    Notification

--- 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -1,35 +1,61 @@
-import React from 'react'
-import './Notification.css'
-import close from '../assets/close.png';
+import React, { Fragment } from 'react';
+import './Notifications.css';
+import close_icon from '../assets/close-icon.png';
 import { getLatestNotification } from '../utils/utils';
+import NotificationItem from './NotificationItem';
+import PropTypes from 'prop-types';
 
-function Notifiaction() {
-    return(
-        <div className="Notification">
-            
-            <button style={{
-                position: 'absolute',
-                right:'5px',
-                top: '15px',
-                background: 'none',
-                border: 'none'
-                }} aria-label='Close' onClick={(e)=> {
-                    console.log('Close button has been clicked')
-                }}>
-                    <img src={close} alt='closeIcon' style={{width:'15px', height: 'auto'}}/>
-                </button>
-                <p>Here is the list of notifications</p>
-                <ul>
-                <li style={{color:'blue'}}
-                    data-priority='default'>New course available</li>
-                    <li style={{color:'red'}}
-                     data-priority='urgent'>New resume available</li>
-                    <li style={{color:'red'}}
-                     dta-priority='urgent' dangerouslySetInnerHTML={{__html: getLatestNotification()}}></li>
-                </ul>
+const Notifications = ({ displayDrawer }) => {
+  return (
+    <Fragment>
+      <div className='menuItem'>
+        <p>Your notifications</p>
+      </div>
+      {displayDrawer && (
+        <div className='Notifications'>
+          <p>Here is the list of notifications</p>
+          <ul>
+            <NotificationItem type='default' value='New course available' />
+            <NotificationItem type='urgent' value='New resume available' />
+            <NotificationItem
+              type='urgent'
+              html={{ __html: getLatestNotification() }}
+            />
+          </ul>
+          <button
+            type='button'
+            aria-label='Close'
+            onClick={() => console.log('Close button has been clicked')}
+            style={{
+              display: 'inline-block',
+              position: 'absolute',
+              top: '56px',
+              right: '16px',
+              background: 0,
+              border: 0,
+              outline: 'none',
+              cursor: 'pointer',
+              zIndex: 1,
+            }}
+          >
+            <img
+              src={close_icon}
+              alt=''
+              style={{ width: '8px', height: '8px' }}
+            />
+          </button>
         </div>
-    )
-}
+      )}
+    </Fragment>
+  );
+};
 
+Notifications.defaultProps = {
+  displayDrawer: false,
+};
 
-export default Notifiaction
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+};
+
+export default Notifications;
