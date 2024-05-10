59df8d62b56c55a4c8ad84929c315eebb54e4061
Ayo <talktojmcvibes@gmail.com>
2024-05-10 21:40:28 +0100

    footer test

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.test.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.test.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Footer from './Footer';
-import { user, logOut, AppContext } from '../App/AppContext';
+import { shallow, mount } from "enzyme";
+import React from "react";
+import { Footer } from "./Footer";
+import AppContext from "../App/AppContext";
+import { user, logOut } from "../App/AppContext";
+import { shallowEqual } from "react-redux";
 
-describe('<Footer />', () => {
-  it('render without crashing', () => {
+describe("<Footer />", () => {
+  it("Footer renders without crashing", () => {
     const wrapper = shallow(<Footer />);
-    expect(wrapper.exists());
+    expect(wrapper.exists()).toEqual(true);
   });
-  /*
-  it('paragraph and content', () => {
-    expect(wrapper.find('footer p')).toHaveLength(1);
-    expect(wrapper.find('footer p').text()).toContain('Copyright');
+  it("Verify that the components at the very least render the text “Copyright”", () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find("div.footer p")).toHaveLength(1);
+    expect(wrapper.find("div.footer p").text()).toContain("Copyright");
   });
-*/
-  it('logged out within the context', () => {
+
+  it("verify that the link is not displayed when the user is logged out within the context", () => {
+    const wrapper = shallow(<Footer user={null} />);
+    expect(wrapper.find("div.footer a")).toHaveLength(0);
+  });
+
+  it("verify that the link is displayed when the user is logged in within the context", () => {
     const wrapper = shallow(
-      <AppContext.Provider value={{ user, logOut }}>
-        <Footer />
-      </AppContext.Provider>
+      <Footer user={{ email: "larry@hd.com", password: "123456" }} />
     );
-    expect(wrapper.find('footer a')).toHaveLength(0);
+    expect(wrapper.find("div.footer a")).toHaveLength(1);
+    expect(wrapper.find("div.footer a").text()).toEqual("Contact us");
   });
 });
