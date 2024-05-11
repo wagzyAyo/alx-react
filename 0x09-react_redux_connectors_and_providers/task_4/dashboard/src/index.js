c5db1524404655fda815807a5ccd75d5d0e3e716
Ayo <talktojmcvibes@gmail.com>
2024-05-11 20:56:34 +0100

    update App.test.js and index.js

--- 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware, compose } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import App from "./App/App";
-import uiReducer, { initialState } from "./reducers/uiReducer";
-import { Map } from "immutable";
+import rootReducer, { initialState } from "./reducers/rootReducer";
 
 // const store = createStore(
 //   uiReducer,
@@ -17,8 +16,8 @@ import { Map } from "immutable";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  uiReducer,
-  Map(initialState),
+  combineReducers(rootReducer),
+  initialState,
   composeEnhancers(applyMiddleware(thunk))
 );
 
